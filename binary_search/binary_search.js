f4ccfbda3e01331a97c77b2e557e7b177d8599c9
notebook da lesli <joao.eudes13jem@gmail.com>
2026-03-29 08:39:56 -0300

    add binary_search/

---
+++ binary_search/binary_search.js
@@ -0,0 +1,14 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let inicio = 0
+let fim = numeros.length - 1
+
+const binary_search = (array, choiceItem) => {
+  while (inicio <= fim) {
+    let meio = (inicio + fim) / 2
+    
+    if (meio === choiceItem) {
+
+    }
+  }
+}
