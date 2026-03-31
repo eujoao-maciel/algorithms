132399a0c0b8db13ce9b1a0539413eedd54aff44
j <joao.eudes13jem@gmail.com>
2026-03-31 02:30:13 -0300

    solved binary_search task

--- binary_search/binary_search.js
+++ binary_search/binary_search.js
@@ -1,14 +1,29 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-let inicio = 0
-let fim = numeros.length - 1
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const binary_search = (array, choiceItem) => {
-  while (inicio <= fim) {
-    let meio = (inicio + fim) / 2
-    
-    if (meio === choiceItem) {
+  let inicio = 0;
+  let fim = numeros.length - 1;
 
+  while (inicio <= fim) {
+    let meio = Math.floor((inicio + fim) / 2);
+    let item = array[meio];
+
+    if (item === choiceItem) {
+      return item;
+    }
+
+    if (item > choiceItem) {
+      fim = meio - 1;
+      console.log(`fim = meio:${meio} - 1`)
+    }
+
+    if (item < choiceItem) {
+      inicio = meio + 1;
+      console.log(`inicio = meio:${meio} + 1`)
     }
   }
-}
+
+  return null;
+};
+
+console.log(binary_search(numeros, 10));
