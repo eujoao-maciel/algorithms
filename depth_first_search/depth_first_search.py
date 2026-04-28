7ea7eb59c7005b3639bc2eb99c70aa3fed07f1dd
j <joao.eudes13jem@gmail.com>
2026-04-28 12:41:36 -0300

    enhanced BFS algorithm

--- depth_first_search/depth_first_search.py
+++ depth_first_search/depth_first_search.py
@@ -1,24 +1,3 @@
-from collections import deque
-
-def pessoa_e_vendedor(nome):
-    return nome[-1] == 'm'
-
-def search(grafo, inicio):
-    fila_de_pesquisa = deque(grafo[inicio]) 
-    
-    while fila_de_pesquisa:
-        pessoa = fila_de_pesquisa.popleft()
-
-        if pessoa_e_vendedor(pessoa):
-            print (f"{pessoa} é um vendedor")
-            return True
-        else:
-            fila_de_pesquisa += grafo[pessoa]
-    
-    return False
-
-
-
 grafo = {}
 grafo["voce"] = ['alice', 'bob', 'claire']
 grafo['bob'] = ['anuj', 'peggy']
@@ -29,5 +8,28 @@ grafo['peggy'] = []
 grafo['thom'] = []
 grafo['jonny'] = []
 
+from collections import deque
+
+def pessoa_e_vendedor(nome):
+    return nome[-1] == 'm'
+
+def search(grafo, inicio):
+    fila_de_pesquisa = deque(grafo[inicio]) 
+    verificadas = []
+    
+    while fila_de_pesquisa:
+        pessoa = fila_de_pesquisa.popleft()
+
+        if not pessoa in verificadas: 
+            if pessoa_e_vendedor(pessoa):
+                print (f"{pessoa} é um vendedor")
+                return True
+            else:
+                fila_de_pesquisa += grafo[pessoa]
+                verificadas.append(pessoa)
+ 
+
+   
+    return False
 
 search(grafo, 'voce')
